9ddd89bd7f61d061f2adc3347aa4375d1b5bd633
Xurd3 <61953663+Xurd3@users.noreply.github.com>
2022-04-18 16:03:57 +0100

    header data

---
+++ src/data/homeData/homeHeader.js
@@ -0,0 +1,30 @@
+//Images(desktop) for header section
+import paramourDesktop from "../../starter-code/assets/home/desktop/image-hero-paramour.jpg";
+import federalDesktop from "../../starter-code/assets/home/desktop/image-hero-federal.jpg";
+import seraphDesktop from "../../starter-code/assets/home/desktop/image-hero-seraph.jpg";
+import trinityDesktop from "../../starter-code/assets/home/desktop/image-hero-trinity.jpg";
+
+//Images for tablet
+import paramourTablet from "../../starter-code/assets/home/tablet/image-hero-paramour.jpg";
+import federalTablet from "../../starter-code/assets/home/tablet/image-hero-federal.jpg";
+import seraphTablet from "../../starter-code/assets/home/tablet/image-hero-seraph.jpg";
+import trinityTablet from "../../starter-code/assets/home/tablet/image-hero-trinity.jpg";
+
+//Images for mobile
+import paramourMobile from "../../starter-code/assets/home/mobile/image-hero-paramour.jpg";
+import federalMobile from "../../starter-code/assets/home/mobile/image-hero-federal.jpg";
+import seraphMobile from "../../starter-code/assets/home/mobile/image-hero-seraph.jpg";
+import trinityMobile from "../../starter-code/assets/home/mobile/image-hero-trinity.jpg";
+
+//HEADER DATA
+export const header = {
+  btnText: "See Our Portfolio",
+  btnGroupText: [01, 02, 03, 04],
+  mainContents: [
+    {
+      heading: "",
+      img: "",
+      paragraph: "",
+    },
+  ],
+};
