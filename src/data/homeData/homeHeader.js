63915830fe2d06c91d3a17f48c671429d5ec59d3
Xurd3 <61953663+Xurd3@users.noreply.github.com>
2022-04-20 14:48:55 +0100

    data for home.js

--- src/data/homeData/homeHeader.js
+++ src/data/homeData/homeHeader.js
@@ -16,14 +16,94 @@ import federalMobile from "../../starter-code/assets/home/mobile/image-hero-fede
 import seraphMobile from "../../starter-code/assets/home/mobile/image-hero-seraph.jpg";
 import trinityMobile from "../../starter-code/assets/home/mobile/image-hero-trinity.jpg";
 
+//Image for welcome
+import welcomeImg from "../../starter-code/assets/home/desktop/image-welcome.jpg";
+
+//Image for about section
+import teamDesktop from "../../starter-code/assets/home/desktop/image-small-team.jpg";
+import teamTablet from "../../starter-code/assets/home/tablet/image-small-team.jpg";
+import teamMobile from "../../starter-code/assets/home/mobile/image-small-team.jpg";
+
 //HEADER DATA
 export const header = {
   btnText: "See Our Portfolio",
   btnGroupText: [01, 02, 03, 04],
   mainContents: [
     {
-      heading: "",
-      img: "",
+      heading: "Project Paramour",
+      desktopImg: paramourDesktop,
+      tabletImg: paramourTablet,
+      mobileImg: paramourMobile,
+      paragraph:
+        "Project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture.",
+    },
+    {
+      heading: "Seraph Station",
+      desktopImg: seraphDesktop,
+      tabletImg: seraphTablet,
+      mobileImg: seraphMobile,
+      paragraph:
+        "The Seraph Station project challenged us to design a unique station that would transport people through time. The result is a fresh and futuristic model inspired by space stations.",
+    },
+    {
+      heading: "Federal II Tower",
+      desktopImg: federalDesktop,
+      tabletImg: federalTablet,
+      mobileImg: federalMobile,
+      paragraph:
+        "A sequel theme project for a tower originally built in the 1800s. We achieved this with a striking look of brutal minimalism with modern touches.",
+    },
+    {
+      heading: "Trinity Bank Tower",
+      desktopImg: trinityDesktop,
+      tabletImg: trinityTablet,
+      mobileImg: trinityMobile,
+      paragraph:
+        "Trinity Bank challenged us to make a concept for a 84 story building located in the middle of a city with a high earthquake frequency. For this project we used curves to blend design and stability to meet our objectives.",
+    },
+  ],
+};
+
+//Sub header section
+export const subHeader = {
+  headingLarge: "Welcome",
+  headingSmall: " Welcome to Arch Studio",
+  text: [
+    "We have aunique network and skillset to help bring your projects to life. Our smallteam of highly skilled individuals combined with our large network put us in a strong position to deliver exceptional results.",
+    "Over the past 10 years, we have worked on all kinds of projects. From stations to high-rise buildings, we create spaces that inspire and delight",
+    "We work closely with our clients so that we understand the intricacies of each project. This allows us to work in harmony the surrounding area to create truly stunning projects that will stand the test of time.",
+  ],
+  img: welcomeImg,
+};
+
+export const about = {
+  heading: "Small team, big ideas",
+  btnText: "About Us",
+  desktopImg: teamDesktop,
+  tabletImg: teamTablet,
+  mobileImg: teamMobile,
+};
+
+export const featured = {
+  heading: "",
+  btnText: "",
+  subContents: [
+    {
+      img: null,
+      headingLarge: "",
+      headingSmall: "",
+      paragraph: "",
+    },
+    {
+      img: null,
+      headingLarge: "",
+      headingSmall: "",
+      paragraph: "",
+    },
+    {
+      img: null,
+      headingLarge: "",
+      headingSmall: "",
       paragraph: "",
     },
   ],
