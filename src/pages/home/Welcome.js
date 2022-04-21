18e1d7534d90e00b4ed543b524db632f50b8f1fd
Xurd3 <61953663+Xurd3@users.noreply.github.com>
2022-04-21 15:35:43 +0100

    added welcome section for home page

---
+++ src/pages/home/Welcome.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <section className="welcome">
+      <h1 className="heading-large"></h1>
+      <div>
+        <h2 className="heading-small"></h2>
+        <div className="text-group">
+          <p className="text-primary"></p>
+          <p className="text-primary"></p>
+          <p className="text-primary"></p>
+        </div>
+      </div>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </section>
+  );
+};
+
+export default Welcome;
