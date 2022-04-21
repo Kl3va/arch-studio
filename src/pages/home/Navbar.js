0eda758681c10d71c5e3170de134a10cefb901b9
Xurd3 <61953663+Xurd3@users.noreply.github.com>
2022-04-21 12:59:23 +0100

    updated data for home

--- src/pages/home/Navbar.js
+++ src/pages/home/Navbar.js
@@ -4,19 +4,21 @@ import { Link } from "react-router-dom";
 const Navbar = ({ logo, navigation }) => {
   return (
     <nav>
-      <div className="nav-logo">
+      <div className="nav__logo">
         <img src={logo} alt="logo" />
       </div>
-      <div className="nav-links">
+      <ul className="nav__links">
         {navigation.map((nav, index) => {
           const { link, page } = nav;
           return (
-            <Link key={index} to={page}>
-              {link}
-            </Link>
+            <li className="nav__items">
+              <Link key={index} to={page}>
+                {link}
+              </Link>
+            </li>
           );
         })}
-      </div>
+      </ul>
     </nav>
   );
 };
